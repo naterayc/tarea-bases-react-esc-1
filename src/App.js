31517c11b04c9463a642d172bcdeca56ca16b137
naterayc <yndinatera10@gmail.com>
2021-12-09 08:54:16 -0300

    crea paginas, navegacion

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import {Routes ,Route} from "react-router-dom"
+
+import Home from './pages/home/home.component';
+import Gallery from './pages/gallery/gallery.component';
+import Details from './pages/details/details.component';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route path='/gallery' element={<Gallery />} />
+          <Route path='/details' element={<Details />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/details/details.component.jsx
@@ -0,0 +1,7 @@
+import React from "react"; 
+
+const Details = () => {
+    return(<p>Details</p>)
+}
+
+export default Details;
